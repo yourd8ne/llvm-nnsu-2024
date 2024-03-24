21861d4906dc6ae7680fdf7bfc88918e730dfe1e
Mikhail Safronov <45272028+yourd8ne@users.noreply.github.com>
2024-03-24 12:52:57 +0300

    Update clang/test/lab1/safronov_mikhail/test.cpp
    Co-authored-by: m-ly4 <mikelitch@gmail.com>

--- clang/test/lab1/safronov_mikhail/test.cpp
+++ clang/test/lab1/safronov_mikhail/test.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -load %llvmshlibdir/PrintClassesPlugin%pluginext -plugin print-classes --help %s 2>&1 | FileCheck %s
+// RUN: not %clang_cc1 -load %llvmshlibdir/PrintClassesPlugin%pluginext -plugin print-classes --help %s 2>&1 | FileCheck %s
 
 // CHECK: Empty
 class Empty {};
