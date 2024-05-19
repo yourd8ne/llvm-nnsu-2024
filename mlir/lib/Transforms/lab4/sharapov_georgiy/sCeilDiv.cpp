83b99328b6329e4e3307f6d246077d6a9f36cc86
nol0n <sharapovegor2003@gmail.com>
2024-05-19 18:46:14 +0300

    Sharapov Georgiy. Lab 4. Option 1. (#144)
    Implemented mlir pass that breaks arith.ceildivui and arith.ceildivsi operations into calculation following the rule: ceildiv(a, b) = (a + b - 1) / b.

---
+++ mlir/lib/Transforms/lab4/sharapov_georgiy/sCeilDiv.cpp
@@ -0,0 +1,66 @@
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Tools/Plugins/PassPlugin.h"
+
+using namespace mlir;
+
+namespace {
+
+enum OpType { SignedOp, UnsignedOp };
+
+class sCeilDivPass : public PassWrapper<sCeilDivPass, OperationPass<ModuleOp>> {
+public:
+  StringRef getArgument() const final { return "sharapov_ceildiv"; }
+  StringRef getDescription() const final {
+    return "breaks arith.ceildivui and arith.ceildivsi operations into arith. "
+           "calculations";
+  }
+
+  void runOnOperation() override {
+    getOperation()->walk([&](Operation *op) {
+      if (auto ceilDivUI = dyn_cast<arith::CeilDivUIOp>(op)) {
+        replaceCeilDiv(ceilDivUI, OpType::UnsignedOp);
+      } else if (auto ceilDivSI = dyn_cast<arith::CeilDivSIOp>(op)) {
+        replaceCeilDiv(ceilDivSI, OpType::SignedOp);
+      }
+    });
+  }
+
+private:
+  template <typename CeilDivOpType>
+  void replaceCeilDiv(CeilDivOpType op, OpType type) {
+    OpBuilder builder(op);
+    Location loc = op.getLoc();
+    Value a = op.getLhs();
+    Value b = op.getRhs();
+
+    Value one =
+        builder.create<arith::ConstantIntOp>(loc, 1, builder.getI32Type());
+    Value add = builder.create<arith::AddIOp>(loc, a, b);
+    Value sub = builder.create<arith::SubIOp>(loc, add, one);
+    Value div;
+
+    if (type == OpType::SignedOp) {
+      div = builder.create<arith::DivSIOp>(loc, sub, b);
+    } else {
+      div = builder.create<arith::DivUIOp>(loc, sub, b);
+    }
+
+    op.replaceAllUsesWith(div);
+    op.erase();
+  }
+};
+} // anonymous namespace
+
+MLIR_DECLARE_EXPLICIT_TYPE_ID(sCeilDivPass)
+MLIR_DEFINE_EXPLICIT_TYPE_ID(sCeilDivPass)
+
+PassPluginLibraryInfo getsCeilDivPassPluginInfo() {
+  return {MLIR_PLUGIN_API_VERSION, "sharapov_ceildiv", LLVM_VERSION_STRING,
+          []() { PassRegistration<sCeilDivPass>(); }};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo mlirGetPassPluginInfo() {
+  return getsCeilDivPassPluginInfo();
+}
---
+++ mlir/test/Transforms/lab4/sharapov_georgiy/test.mlir
@@ -0,0 +1,30 @@
+// RUN: mlir-opt -load-pass-plugin=%mlir_lib_dir/sCeilDiv%shlibext --pass-pipeline="builtin.module(sharapov_ceildiv)" %s | FileCheck %s
+
+module {
+  llvm.func @ceildivsi(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+    // CHECK: llvm.func @ceildivsi(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+    // CHECK-NEXT: %c1_i32 = arith.constant 1 : i32
+    // CHECK-NEXT: %[[add:.*]] = arith.addi %arg0, %arg1 : i32
+    // CHECK-NEXT: %[[sub:.*]] = arith.subi %[[add:.*]], %c1_i32 : i32
+    // CHECK-NEXT: %[[div:.*]] = arith.divsi %[[sub]], %arg1 : i32
+    // CHECK-NOT: %0 = arith.ceildivsi %arg0, %arg1 : i32
+    %0 = arith.ceildivsi %arg0, %arg1 : i32
+    llvm.return %0 : i32
+  }
+  llvm.func @ceildivui(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+    // CHECK: llvm.func @ceildivui(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+    // CHECK-NEXT: %c1_i32 = arith.constant 1 : i32
+    // CHECK-NEXT: %[[add:.*]] = arith.addi %arg0, %arg1 : i32
+    // CHECK-NEXT: %[[sub:.*]] = arith.subi %[[add]], %c1_i32 : i32
+    // CHECK-NEXT: %[[div:.*]] = arith.divui %[[sub]], %arg1 : i32
+    // CHECK-NOT: %0 = arith.ceildivui %arg0, %arg1 : i32
+    %0 = arith.ceildivui %arg0, %arg1 : i32
+    llvm.return %0 : i32
+  }
+  llvm.func @example(%arg0: i32, %arg1: i32, %arg2: i32, %arg3: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+    %0 = llvm.call @ceildivsi(%arg0, %arg1) : (i32, i32) -> i32
+    %1 = llvm.call @ceildivui(%arg2, %arg3) : (i32, i32) -> i32
+    %2 = arith.addi %0, %1 : i32
+    llvm.return %2 : i32
+  }
+}
