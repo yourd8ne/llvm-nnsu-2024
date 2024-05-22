5e25f1feb237009df2dede42f4ba14e5b21dc522
yourd8ne <done101@mail.ru>
2024-05-22 15:54:03 +0300

    fix clang-format

--- llvm/labs/lab2/safronov_mikhail/Instrumentation_func.cpp
+++ llvm/labs/lab2/safronov_mikhail/Instrumentation_func.cpp
@@ -6,70 +6,78 @@
 #include "llvm/Support/raw_ostream.h"
 
 struct InstrumentFunctions : llvm::PassInfoMixin<InstrumentFunctions> {
-    llvm::PreservedAnalyses run(llvm::Function &func, llvm::FunctionAnalysisManager &) {
-        llvm::LLVMContext &context = func.getContext();
-        llvm::IRBuilder<> builder(context);
-        auto module = func.getParent();
+  llvm::PreservedAnalyses run(llvm::Function &func,
+                              llvm::FunctionAnalysisManager &) {
+    llvm::LLVMContext &context = func.getContext();
+    llvm::IRBuilder<> builder(context);
+    auto module = func.getParent();
 
-        llvm::FunctionType *funcType = llvm::FunctionType::get(llvm::Type::getVoidTy(context), false);
-        llvm::FunctionCallee instrStartFunc = module->getOrInsertFunction("instrument_start", funcType);
-        llvm::FunctionCallee instEndFunc = module->getOrInsertFunction("instrument_end", funcType);
-        
-        bool startInserted = false;
-        bool endInserted = false;
+    llvm::FunctionType *funcType =
+        llvm::FunctionType::get(llvm::Type::getVoidTy(context), false);
+    llvm::FunctionCallee instrStartFunc =
+        module->getOrInsertFunction("instrument_start", funcType);
+    llvm::FunctionCallee instEndFunc =
+        module->getOrInsertFunction("instrument_end", funcType);
 
-        llvm::Function *func_f_f = llvm::dyn_cast<llvm::Function>(instrStartFunc.getCallee());
-        llvm::Function *func_l_f = llvm::dyn_cast<llvm::Function>(instEndFunc.getCallee());
+    bool startInserted = false;
+    bool endInserted = false;
 
-        // Проверка, добавлен ли уже вызов instrument_start
-        for (auto *U : func_f_f->users()) {
-            if (auto *callInst = llvm::dyn_cast<llvm::CallInst>(U)) {
-                if (callInst->getFunction() == &func) {
-                    startInserted = true;
-                }
-            }
+    llvm::Function *func_f_f =
+        llvm::dyn_cast<llvm::Function>(instrStartFunc.getCallee());
+    llvm::Function *func_l_f =
+        llvm::dyn_cast<llvm::Function>(instEndFunc.getCallee());
+
+    // Проверка, добавлен ли уже вызов instrument_start
+    for (auto *U : func_f_f->users()) {
+      if (auto *callInst = llvm::dyn_cast<llvm::CallInst>(U)) {
+        if (callInst->getFunction() == &func) {
+          startInserted = true;
         }
-
-        // Проверка, добавлен ли уже вызов instrument_end
-        for (auto *U : func_l_f->users()) {
-            if (auto *callInst = llvm::dyn_cast<llvm::CallInst>(U)) {
-                if (callInst->getFunction() == &func) {
-                    endInserted = true;
-                }
-            }
-        }
-
-        if (!startInserted) {
-            builder.SetInsertPoint(&func.getEntryBlock().front());
-            builder.CreateCall(instrStartFunc);
-        }
-
-        if (!endInserted) {
-            for (auto &block : func) {
-                if (llvm::isa<llvm::ReturnInst>(block.getTerminator())) {
-                    builder.SetInsertPoint(block.getTerminator());
-                    builder.CreateCall(instEndFunc);
-                }
-            }
-        }
-
-        return llvm::PreservedAnalyses::all();
+      }
     }
+
+    // Проверка, добавлен ли уже вызов instrument_end
+    for (auto *U : func_l_f->users()) {
+      if (auto *callInst = llvm::dyn_cast<llvm::CallInst>(U)) {
+        if (callInst->getFunction() == &func) {
+          endInserted = true;
+        }
+      }
+    }
+
+    if (!startInserted) {
+      builder.SetInsertPoint(&func.getEntryBlock().front());
+      builder.CreateCall(instrStartFunc);
+    }
+
+    if (!endInserted) {
+      for (auto &block : func) {
+        if (llvm::isa<llvm::ReturnInst>(block.getTerminator())) {
+          builder.SetInsertPoint(block.getTerminator());
+          builder.CreateCall(instEndFunc);
+        }
+      }
+    }
+
+    return llvm::PreservedAnalyses::all();
+  }
 };
 
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
-    llvm::errs() << "Loading InstrumentFunctions Plugin\n";  // Отладочный вывод
-    return {LLVM_PLUGIN_API_VERSION, "instr_func", "0.1",
-            [](llvm::PassBuilder &PB) {
-                PB.registerPipelineParsingCallback(
-                    [](llvm::StringRef name, llvm::FunctionPassManager &FPM,
-                       llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) -> bool {
-                        if (name == "instr_func") {
-                            llvm::errs() << "Registering InstrumentFunctions Pass\n";  // Отладочный вывод
-                            FPM.addPass(InstrumentFunctions{});
-                            return true;
-                        }
-                        return false;
-                    });
-            }};
+  llvm::errs() << "Loading InstrumentFunctions Plugin\n"; // Отладочный вывод
+  return {LLVM_PLUGIN_API_VERSION, "instr_func", "0.1",
+          [](llvm::PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](llvm::StringRef name, llvm::FunctionPassManager &FPM,
+                   llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) -> bool {
+                  if (name == "instr_func") {
+                    llvm::errs()
+                        << "Registering InstrumentFunctions Pass\n"; // Отладочный
+                                                                     // вывод
+                    FPM.addPass(InstrumentFunctions{});
+                    return true;
+                  }
+                  return false;
+                });
+          }};
 }
