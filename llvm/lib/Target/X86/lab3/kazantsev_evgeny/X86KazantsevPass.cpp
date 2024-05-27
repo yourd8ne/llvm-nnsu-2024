4724a4fdbe6e200eb5f64db848a5d778ef11533e
neotouch1 <90023755+neotouch1@users.noreply.github.com>
2024-05-27 16:00:08 +0300

    first_attem (#134)
    A plugin was made that replaces the addition and multiplication instructions with fma

---
+++ llvm/lib/Target/X86/lab3/kazantsev_evgeny/X86KazantsevPass.cpp
@@ -0,0 +1,83 @@
+#include "X86.h"
+#include "X86InstrInfo.h"
+#include "llvm/CodeGen/MachineBasicBlock.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineInstr.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/Register.h"
+#include <vector>
+
+using namespace llvm;
+
+namespace {
+class X86KazantsevPass : public MachineFunctionPass {
+public:
+  static char ID;
+
+  X86KazantsevPass() : MachineFunctionPass(ID) {}
+
+  bool runOnMachineFunction(MachineFunction &MF) override;
+
+  StringRef getPassName() const override { return "X86 Kazantsev Pass"; }
+};
+} // namespace
+
+char X86KazantsevPass::ID = 0;
+
+bool X86KazantsevPass::runOnMachineFunction(MachineFunction &MF) {
+  bool Modified = false;
+  const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
+
+  for (auto &MBB : MF) {
+    std::vector<llvm::MachineBasicBlock::iterator> mulToErase;
+    llvm::Register ar2;
+    MachineInstr *AI;
+    for (auto I = MBB.begin(); I != MBB.end(); I++) {
+      if (I->getOpcode() != X86::MULPDrr) {
+        continue;
+      }
+      const llvm::Register mr0 = I->getOperand(0).getReg();
+      bool Found = false;
+      for (auto J = I; J != MBB.end(); J++) {
+        if (J->getOpcode() != X86::ADDPDrr) {
+          continue;
+        }
+        llvm::Register ar1 = J->getOperand(1).getReg();
+        ar2 = J->getOperand(2).getReg();
+        if (mr0 != ar1 && mr0 != ar2)
+          continue;
+        if (Found) {
+          Found = false;
+          break;
+        }
+        if (mr0 == ar2) {
+          ar2 = ar1;
+        }
+        AI = &(*J);
+        Found = true;
+      }
+      if (!Found)
+        continue;
+
+      auto &MI = *I;
+      MIMetadata MIMD(*AI);
+      MachineInstrBuilder MIB =
+          BuildMI(MBB, *AI, MIMD, TII->get(X86::VFMADD213PDr));
+      MIB.addReg(AI->getOperand(0).getReg(), RegState::Define);
+      MIB.addReg(MI.getOperand(1).getReg());
+      MIB.addReg(MI.getOperand(2).getReg());
+      MIB.addReg(ar2);
+      AI->eraseFromParent();
+      mulToErase.emplace_back(I);
+      Modified = true;
+    }
+    for (auto &mul : mulToErase) {
+      (*mul).eraseFromParent();
+    }
+  }
+
+  return Modified;
+}
+
+static RegisterPass<X86KazantsevPass> X("x86-kazantsev-pass",
+                                        "X86 Kazantsev Pass", false, false);
